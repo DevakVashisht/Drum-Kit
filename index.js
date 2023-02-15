ce33d8b762a003d21723943aed7f6323d0331607
Devak Vashisht <120743172+DevakVashisht@users.noreply.github.com>
2023-02-15 15:40:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ for (var i = 0; i < numberOfDrumButtons; i++){
     var buttonHtml = this.innerHTML
     switch (buttonHtml) {
         case "w":
-            tom1 = new Audio("sounds/tom-1.mp3");
+            tom1 = new Audio("tom-1.mp3");
             tom1.play();
             break;
         case "a":
-            tom2 = new Audio("sounds/tom-2.mp3");
+            tom2 = new Audio("tom-2.mp3");
             tom2.play();
             break;
         case "s":
-            tom3 = new Audio("sounds/tom-3.mp3");
+            tom3 = new Audio("tom-3.mp3");
             tom3.play();
             break;
         case "d":
@@ -21,15 +21,15 @@ for (var i = 0; i < numberOfDrumButtons; i++){
             tom4.play();
             break;
         case "j":
-            snare = new Audio("sounds/snare.mp3");
+            snare = new Audio("snare.mp3");
             snare.play();
             break;
         case "k":
-            crash = new Audio("sounds/crash.mp3");
+            crash = new Audio("scrash.mp3");
             crash.play();
             break;
         case "l":
-            kickBass = new Audio("sounds/kick-bass.mp3");
+            kickBass = new Audio("kick-bass.mp3");
             kickBass.play();
             break;
     
@@ -45,11 +45,11 @@ document.addEventListener("keydown", function(e){
 function soundMaker(key){
     switch (key) {
         case "w":
-            tom1 = new Audio("sounds/tom-1.mp3");
+            tom1 = new Audio("tom-1.mp3");
             tom1.play();
             break;
         case "a":
-            tom2 = new Audio("sounds/tom-2.mp3");
+            tom2 = new Audio("tom-2.mp3");
             tom2.play();
             break;
         case "s":
@@ -57,19 +57,19 @@ function soundMaker(key){
             tom3.play();
             break;
         case "d":
-            tom4 = new Audio("sounds/tom-4.mp3");
+            tom4 = new Audio("tom-4.mp3");
             tom4.play();
             break;
         case "j":
-            snare = new Audio("sounds/snare.mp3");
+            snare = new Audio("snare.mp3");
             snare.play();
             break;
         case "k":
-            crash = new Audio("sounds/crash.mp3");
+            crash = new Audio("crash.mp3");
             crash.play();
             break;
         case "l":
-            kickBass = new Audio("sounds/kick-bass.mp3");
+            kickBass = new Audio("kick-bass.mp3");
             kickBass.play();
             break;
     
